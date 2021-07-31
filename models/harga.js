80bc9bf4cb18d1d3a89486f7d08f3e6473a0f541
Thefan <9beat7@gmail.com>
2021-07-31 14:49:32 +0700

    harga

---
+++ models/harga.js
@@ -0,0 +1,12 @@
+const harga = {
+  kelas: [
+    { single: 100000 },
+    { vip: 250000 }
+  ],
+  fasilitas: [
+    { extra_bed: 50000 },
+    { cleaning: 25000 }
+  ]
+}
+
+module.exports = harga
--- routes/index-router.js
+++ routes/index-router.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+const harga = require('../models/harga')
+
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { harga: harga })
 })
 
 module.exports = router

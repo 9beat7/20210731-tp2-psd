232b005c73c6f52181df4e07a14d1215efbbed8f
Thefan <9beat7@gmail.com>
2021-07-31 14:00:53 +0700

    buat server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const port = 3000
+
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000, () => {
+  console.log(`Aplikasi berjalan pada http://localhost:${port}`)
+})
